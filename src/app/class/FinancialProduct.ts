4d8ad7da158a42e6d86f6e5f10ad7b03008d61dd
Julio Barreto <e_jcbarreto+coppel@bancoppel.com>
2025-07-14 21:53:10 -0500

    feat(test-frontend): US00 | create builder product

---
+++ src/app/class/FinancialProduct.ts
@@ -0,0 +1,51 @@
+import {IFinancialProducts} from "@app/models/IFinancialProducts";
+
+export class FinancialProduct implements IFinancialProducts{
+    id!: number;
+    name!: string;
+    description!: string;
+    logo!: string;
+    date_release!: string;
+    date_revision!: string;
+}
+
+export class FinancialProductsBuilder {
+    financialProduct: FinancialProduct;
+
+    constructor() {
+        this.financialProduct = new FinancialProduct();
+    }
+    setId(value: number): this {
+        this.financialProduct.id = value;
+        return this;
+    }
+
+    setName(value: string): this {
+        this.financialProduct.name = value;
+        return this;
+    }
+
+    setDescription(value: string): this {
+        this.financialProduct.description = value;
+        return this;
+    }
+
+    setLogo(value: string): this {
+        this.financialProduct.logo = value;
+        return this;
+    }
+
+    setDateRelease(value: string): this {
+        this.financialProduct.date_release = value;
+        return this;
+    }
+
+    setDateRevision(value: string): this {
+        this.financialProduct.date_revision = value;
+        return this;
+    }
+
+    build(): FinancialProduct {
+        return this.financialProduct;
+    }
+}
